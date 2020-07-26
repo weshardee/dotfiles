9234a032b6954a40ea7967ccdb52ae79fc6245e5
Wes Hardee <weshardee@gmail.com>
2020-07-25 21:29:53 -0700

    remove font

--- run.sh
+++ run.sh
@@ -8,11 +8,14 @@ ln -sf ${PWD}/zshrc ~/.zshrc
 # make sure homebrew is installed
 [ ! brew ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# install command line tools
 [ ! z ] && brew install z
 [ ! fzf ] && brew install fzf
 [ ! nvim ] && brew install nvim
+[ ! ag ] && brew install the_silver_searcher
+[ ! ranger ] && brew install ranger
 
 # vim plugins
-[ ! "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+[ ! "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
